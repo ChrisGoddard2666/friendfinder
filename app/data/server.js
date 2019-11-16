a88eddd446cf8ccb8013a48e1544ff9acd845eae
unknown <chrisgoddard1979@gmail.com>
2019-11-16 12:34:42 -0800

    added misplaced javaScript files

--- app/data/server.js
+++ app/data/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/htmlroutes")(app);
 
 // =============================================================================
 // LISTENER
